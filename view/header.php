785f080b57a310df905b0c703a221f72d5093bb2
BrianFenn <34203314+BrianFenn@users.noreply.github.com>
2020-03-15 11:36:39 -0700

    Add files via upload

---
+++ view/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title>Zippy Used Autos</title>
+    <link rel="stylesheet" type="text/css"
+          href="view/main.css">
+          <meta name="viewport" content="width=device-width, initial-scale=1">          
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>Zippy's Used Auto Sales</h1></header>
