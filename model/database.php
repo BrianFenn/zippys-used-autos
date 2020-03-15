2670c8e3bcc9c03a471751ece45b1c38cb06021f
BrianFenn <34203314+BrianFenn@users.noreply.github.com>
2020-03-15 11:50:15 -0700

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
-    $username = 'root';
-    $password = 'Kholmes1#';
+    $dsn = 'mysql:host=dyud5fa2qycz1o3v.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=v81fh0xf9phpukqa';
+    $username = 's6hatnjwkzgiq33x';
+    $password = 'i1o53knv8g93sbhl';
 
     try {
         $db = new PDO($dsn, $username, $password);
