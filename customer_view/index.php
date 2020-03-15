ba0d9b8bb7316e2cf558e32509d5c5a39749cf00
BrianFenn <34203314+BrianFenn@users.noreply.github.com>
2020-03-15 11:37:51 -0700

    Add files via upload

---
+++ customer_view/index.php
@@ -0,0 +1,62 @@
+<?php
+    require('../model/database.php');
+    require('../model/vehicle_db.php');
+    require('../model/class_db.php');
+    require('../model/type_db.php');
+    require('../model/make_db.php');
+
+    $action = filter_input(INPUT_POST, 'action');
+    if ($action == NULL) {
+        $action = filter_input(INPUT_GET, 'action');
+        if ($action == NULL) {
+            $action = 'list_vehicles';
+        }
+    }
+
+       
+    if ($action == 'list_vehicles') {
+        $Class_code = filter_input(INPUT_GET, 'Class_code', FILTER_VALIDATE_INT);
+        $Type_code = filter_input(INPUT_GET, 'Type_code', FILTER_VALIDATE_INT);
+        $Make = filter_input(INPUT_GET, 'Make');
+        $Sort = filter_input(INPUT_GET, 'Sort');
+
+        $Vehicle_Makes = get_vehicle_makes();
+
+        
+        // call the functions
+        $Class_name = get_vehicle_class_name($Class_code);
+        $Vehicle_Classes = get_vehicle_classes();
+
+        $Types = get_vehicle_types();
+        $Type_name = get_vehicle_type_name($Type_code);
+
+        if ($Class_code != NULL || $Class_code != FALSE){
+            $Vehicles = get_vehicles_by_class($Class_code);
+        } else if ($Type_code!= NULL || $Type_code) {
+            $Vehicles = get_vehicles_by_type($Type_code);
+        } else if ($Make != NULL | $Make != FALSE) {
+            $Vehicles = get_vehicles_by_make($Make);
+        } else {
+            $Vehicles = get_all_vehicles();
+        }
+
+        
+        
+        
+        //$Vehicles = get_vehicles_by_class($Class_code);
+        
+        //if ( $Class_code != 0 && $Type_code != 0 && $Make != 0) {
+            //$Vehicles = get_vehicles_by_class($Class_code);
+            //$Vehicles = get_all_vehicles(get_vehicles_by_class($Class_code,get_vehicles_by_type($Type_code,get_vehicles_by_make($Make))));
+        //}
+
+        //$Vehicles = get_vehicles_by_selection(); 
+        //Refined_Vehicle_Results = get_all_vehicles(get_vehicles_by_class(get_vehicles_by_type(get_vehicles_by_make())));
+        //$Vehicles = get_all_vehicles(get_vehicles_by_class($Class_code,get_vehicles_by_type($Type_code,get_vehicles_by_make($Make))));
+        
+        include('vehicle_list.php');
+    }
+
+?> 
+
+   
---
+++ customer_view/vehicle_list.php
@@ -0,0 +1,129 @@
+<?php include '../view/header_customer_view.php';
+
+?>
+
+
+
+<main>
+    <h1>Vehicle Inventory List</h1>
+    <?php if ( sizeof($Vehicle_Classes) != 0) { ?>
+                <form action="." method="get" id="category_selection">
+                <div id="hide_select">
+                <label>Class:</label>
+                <select name="Class_code">
+                    <option  onclick="hideMenu()" value="0">View All Classes</option>
+                    <?php foreach ($Vehicle_Classes as $Class) : ?>
+                        <option onclick="hideMenu()" value="<?php echo $Class['Class_code']; ?>">
+                            <?php echo $Class['Class_name']; ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select> 
+                <?php } ?>
+            
+
+            <?php if ( sizeof($Types) != 0) { ?>
+                
+                <label>Type:</label>
+                <select name="Type_code">
+                    <option onclick="hideMenu()"  value="0">View All Types</option>
+                    <?php foreach ($Types as $Type) : ?>
+                        <option onclick="hideMenu()"  value="<?php echo $Type['Type_code']; ?>">
+                            <?php echo $Type['Type_name']; ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select> 
+                <?php } ?>
+
+            <?php if ( sizeof($Vehicle_Makes) != 0) { ?>
+              
+                <label>Make:</label>
+                <select name="Make" >
+                    <option onclick="hideMenu()" value="0">View All Makes</option>
+                    <?php foreach ($Vehicle_Makes as $Make) : ?>
+                        <option onclick="hideMenu()" value="<?php echo $Make['Make']; ?>">
+                            <?php echo $Make['Make']; ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select> 
+                        <!--END OF hide_select-->
+                        </div>
+
+                <p>Sort by:</p>
+                    <input type="radio" id="category_selection" name="Sort" value="year">
+                    <label for="year">Year</label><br>
+                    <input type="radio" id="category_selection" name="Sort" value="price" checked= "checked">
+                    <label for="price">Price</label><br>
+
+                <?php } ?>
+
+                <input type="submit" value="Submit" id="button" class="button blue">
+                </form>
+            
+
+
+
+        <!-- display a table of vehicles -->
+       
+      
+
+<h2><?php 
+if ($Class_code != NULL || $Class_code != FALSE) {
+    //$category_id = 1;
+    //$all_items = get_all_items();
+    //echo "All Vehicles";
+    //echo $Class_name;
+    //echo $Class['Class_code']; 
+} elseif ($Type_code != NULL || $Type_code != FALSE ){
+//echo $Type_name;
+}
+elseif ($Make != NULL || $Make != FALSE ){
+    //echo $Type_name;
+    }
+else{
+    echo "All Vehicles";
+    }
+
+
+        
+        ?>
+
+    <section>    
+        </h2>
+        <div id="table-scroll">
+        <table>
+            <tr>
+                
+                <th>Year</th>
+                <th>Make</th>
+                <th>Model</th>
+                <th>Price</th>
+                <th>Type</th>
+                <th class="right">Class</th>
+                
+            </tr>
+            <?php foreach ($Vehicles as $Vehicle) : ?>
+            <tr>
+                
+                <td><?php echo $Vehicle['Vehicle_year']; ?></td>
+                <td><?php echo $Vehicle['Make']; ?></td>
+                <td><?php echo $Vehicle['Model']; ?></td>
+                <td><?php echo $Vehicle['Price']; ?></td>
+                <td><?php echo $Vehicle['Type_name']; ?></td>
+                <td class="right"><?php echo $Vehicle['Class_name']; ?></td>
+                
+            </tr>
+            <?php endforeach; ?>
+        </table>
+            </div>
+        
+    </section>
+</main>
+
+<script>
+function hideMenu() {
+    var x = document.getElementById("hide_select");
+    x.style.display = "none";
+}
+</script>
+
+<?php include '../view/footer.php'; ?>
